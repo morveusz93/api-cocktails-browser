0a80f88688766f2562b8b91f9a9edcedfaaab1b7
morveusz93 <morveusz93@gmail.com>
2021-05-03 16:13:36 +0200

    added random cocktail

--- cocktails-browser.py
+++ cocktails-browser.py
@@ -25,12 +25,48 @@ def print_drink_info(drinks):
         print("\n")
 
 
+def random_cocktail():
+    response = requests.get('http://www.thecocktaildb.com/api/json/v1/1/random.php').json()
+    # print(json.dumps(response, indent=4))
+    return response
+
+
+def menu():
+    print("[1] - Search by coctail name")
+    print("[2] - Random coctail")
+    print("[0] - Leave")
+    return input()
+
 
 def main():
-    # drinks must be the
-    drinks = ask_name()["drinks"]
-    print_drink_info(drinks)    
-
+    print("Welcome to Coctail Browser!\n")
+    user_choice = 'm'
+    while True:
+        # print menu
+        if user_choice == 'm':
+            user_choice = menu()
+        elif user_choice == '1':
+            # search coctail by name
+            drinks = ask_name()["drinks"]
+            if drinks != None:
+                print_drink_info(drinks)    
+            else:
+                # there is no result with this name
+                print("\nWe found nothing. Try somthing else\n")
+            user_choice = menu()
+        elif user_choice == '2':
+            random_drink = random_cocktail()["drinks"]
+            if random_drink != None:
+                print_drink_info(random_drink)    
+            else:
+                # there is no result with this name
+                print("We found nothing. Try somthing else\n")
+            user_choice = menu()
+        elif user_choice == '0':
+            break
+        else:
+            print("Wrong choice")
+            user_choice = menu()
 
 
 if __name__ == '__main__':
